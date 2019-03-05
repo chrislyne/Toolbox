ef38522488452808e7e57f53373cf1307ac7945c
GOKU <chris@funnylyne.com>
2019-03-05 21:47:25 +1300

    Asset manager interactions

--- pipelime/lm_assetManager.py
+++ pipelime/lm_assetManager.py
@@ -8,12 +8,114 @@ from PySide2 import QtGui
 from PySide2 import QtWidgets
 from PySide2 import QtCore
 import os
+import Tools.screenshot as screenshot
+from stat import S_ISREG, ST_MTIME, ST_MODE
+
+
+def takeScreenshot(assetName,projectsDict):
+    global lch
+    #assetName = assetManagerUIWindow.mainWidget.listWidget_assets.currentItem().text()
+    assetType = assetManagerUIWindow.mainWidget.listWidget_assetType.currentItem().text()
+    project = assetManagerUIWindow.mainWidget.project_comboBox.currentText()
+    assetPath = '%s/scenes/REF/%s/%s'%(projectsDict[project]["projectPath"],assetType,assetName)
+
+    lch = screenshot.ScreenShot()
+    imageFileName = '%s/.data/%s_thumb.jpg'%(assetPath,assetName)
+    print imageFileName
+    lch.launch(imageFileName)
+
+    try:
+        buttonIcon = QtGui.QIcon("%s/icons/%s.jpg"%(qtBase.self_path(), "tap"))
+        window.mainWidget.pushButton_thumb.setIcon(buttonIcon)
+        window.mainWidget.pushButton_thumb.setText("")
+    except:
+        pass
+
+def orderByModified(dirpath):
+
+    # get all entries in the directory w/ stats
+    entries = (os.path.join(dirpath, fn) for fn in os.listdir(dirpath))
+    entries = ((os.stat(path), path) for path in entries)
+    
+    # leave only regular files, insert creation date
+    entries = ((stat[ST_MTIME], path)
+               for stat, path in entries if S_ISREG(stat[ST_MODE]))
+    
+    recentFiles = []
+    
+    for cdate, path in sorted(entries):
+        #print time.ctime(cdate), os.path.basename(path)
+        recentFiles.append(os.path.basename(path))
+    return recentFiles
+
+def newAsset(projectsDict):
+    newAssetType = assetManagerUIWindow.mainWidget.lineEdit_newAssetType.text()
+    newAssetName = assetManagerUIWindow.mainWidget.lineEdit_newAssetName.text()
+    if not newAssetType:
+        newAssetType = assetManagerUIWindow.mainWidget.listWidget_assetType.currentItem().text()
+    project = assetManagerUIWindow.mainWidget.project_comboBox.currentText()
+    assetPath = '%s/scenes/REF/%s/%s'%(projectsDict[project]["projectPath"],newAssetType,newAssetName)
+    print newAssetName
+
+    if not os.path.exists(assetPath):
+        os.makedirs(assetPath)
+    cmds.file(newFile=True, force=True)
+    cmds.file(rename='%s/%s_v001_cl.mb'%(assetPath,newAssetName))
+
+def editAsset(assetName,projectsDict):
+    assetType = assetManagerUIWindow.mainWidget.listWidget_assetType.currentItem().text()
+    project = assetManagerUIWindow.mainWidget.project_comboBox.currentText()
+    assetPath = '%s/scenes/REF/%s/%s'%(projectsDict[project]["projectPath"],assetType,assetName)
+    #find latest version
+    workingFiles = orderByModified(assetPath)
+    if workingFiles:
+        #open existing file
+        latestFilePath = '%s/%s'%(assetPath,workingFiles[-1])
+        cmds.file(latestFilePath, open=True, ignoreVersion=True, force=True)
+    else:
+        #create new file
+        cmds.file(newFile=True, force=True)
+        cmds.file(rename='%s/%s_v001_cl.mb'%(assetPath,assetName))
+
+
+
+def referenceAsset(assetName,projectsDict,imp):
+    #loop through selection
+    for item in assetName:
+        name = item.text()
+        assetType = assetManagerUIWindow.mainWidget.listWidget_assetType.currentItem().text()
+        project = assetManagerUIWindow.mainWidget.project_comboBox.currentText()
+        assetPath = '%s/scenes/REF/%s/%s_REF.mb'%(projectsDict[project]["projectPath"],assetType,name)
+        #try to load the asset file
+        try:
+            if imp != 1:
+                cmds.file(assetPath, reference=True)
+            else:
+                cmds.file(assetPath, i=True)
+        except:
+            print 'unable to load reference %s'%assetPath
+
+def setAsset(assetName,projectsDict):
+
+    assetType = assetManagerUIWindow.mainWidget.listWidget_assetType.currentItem().text()
+    project = assetManagerUIWindow.mainWidget.project_comboBox.currentText()
+    assetPath = '%s/scenes/REF/%s/%s/.data/%s_thumb.jpg'%(projectsDict[project]["projectPath"],assetType,assetName,assetName)
+
+    try:
+        buttonIcon = QtGui.QIcon(assetPath)
+        assetManagerUIWindow.mainWidget.pushButton_thumb.setIcon(buttonIcon)
+        assetManagerUIWindow.mainWidget.pushButton_thumb.setText("")
+        #assetManagerUIWindow.mainWidget.pushButton_thumb.setMaximumWidth(300)
+        #assetManagerUIWindow.mainWidget.pushButton_thumb.setMaximumHeight(300)
+        #assetManagerUIWindow.mainWidget.pushButton_thumb.setIconSize(QtCore.QSize(300,300))
+        
+    except:
+        pass
 
 def setType(key,projectsDict):
     project = assetManagerUIWindow.mainWidget.project_comboBox.currentText()
     typePath = '%s/scenes/REF/%s'%(projectsDict[project]["projectPath"],key)
 
-    print typePath
     assetFolders = []
     files = os.listdir(typePath)
     for name in files:
@@ -23,7 +125,14 @@ def setType(key,projectsDict):
                 assetFolders.append(name)
 
     assetManagerUIWindow.mainWidget.listWidget_assets.clear()
-    assetManagerUIWindow.mainWidget.listWidget_assets.addItems(assetFolders)
+    #assetManagerUIWindow.mainWidget.listWidget_assets.addItems(assetFolders)
+
+    for assets in assetFolders:
+        itm = QtWidgets.QListWidgetItem(QtGui.QIcon('C:/Users/Admin/Documents/Toolbox/icons/lightRed.png'),assets);
+        if os.path.isfile('%s/%s_REF.mb'%(typePath,assets)):
+            itm = QtWidgets.QListWidgetItem(QtGui.QIcon('C:/Users/Admin/Documents/Toolbox/icons/lightGreen.png'),assets);
+
+        assetManagerUIWindow.mainWidget.listWidget_assets.addItem(itm);
 
 
 def setProject(key,projectsDict):
@@ -83,12 +192,17 @@ def assetManagerUI():
 
     window.mainWidget.listWidget_assetType.currentTextChanged.connect(lambda: setType(window.mainWidget.listWidget_assetType.currentItem().text(),projectsDict))
 
-    try:
-        buttonIcon = QtGui.QIcon("%s/icons/%s.jpg"%(qtBase.self_path(), "tap"))
-        window.mainWidget.pushButton_thumb.setIcon(buttonIcon)
-        window.mainWidget.pushButton_thumb.setText("")
-    except:
-        pass
+    window.mainWidget.listWidget_assets.currentTextChanged.connect(lambda: setAsset(window.mainWidget.listWidget_assets.currentItem().text(),projectsDict))
+    #reference button
+    window.mainWidget.pushButton_reference.clicked.connect(lambda: referenceAsset(window.mainWidget.listWidget_assets.selectedItems(),projectsDict,0))
+    #import button
+    window.mainWidget.pushButton_import.clicked.connect(lambda: referenceAsset(window.mainWidget.listWidget_assets.selectedItems(),projectsDict,1))
+    #new button
+    window.mainWidget.pushButton_new.clicked.connect(lambda: newAsset(projectsDict))
+    #edit button
+    window.mainWidget.pushButton_edit.clicked.connect(lambda: editAsset(window.mainWidget.listWidget_assets.currentItem().text(),projectsDict))
+    #thumbnail button
+    window.mainWidget.pushButton_thumb.clicked.connect(lambda: takeScreenshot(window.mainWidget.listWidget_assets.currentItem().text(),projectsDict))
 
     return window
 
