8111486307c3d08aa711230516bebe69be5b00cb
Chris Lyne <chris@funnylyne.com>
2020-08-06 23:12:18 +1200

    Started point direction for ctrl

--- ramenRig/convert_qrc.bat
+++ ramenRig/convert_qrc.bat
@@ -1,3 +1,3 @@
 
-"c:\Program Files\Autodesk\Maya2018\bin\pyside2-rcc.exe" -o C:\Users\Admin\Documents\Toolbox\ramenRig\createCtrl_resources.py C:\Users\Admin\Documents\Toolbox\ramenRig\createCtrl.qrc
+"c:\Program Files\Autodesk\Maya2018\bin\pyside2-rcc.exe" -o C:\Users\Chris\Dropbox\Projects\Toolbox\ramenRig\createCtrl_resources.py C:\Users\Chris\Dropbox\Projects\Toolbox\ramenRig\createCtrl.qrc
 pause
